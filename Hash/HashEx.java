b00c086d68d9833afb0ef4475541422ddbe112fa
Nickolodeon98 <sjeon0730@gmail.com>
2022-10-01 23:32:30 +0900

    Basic class variables added for HashImplementation class

--- Hash/HashEx.java
+++ Hash/HashEx.java
@@ -3,13 +3,28 @@ package Hash;
 import java.util.Hashtable;
 import java.util.Scanner;
 
+class HashImplementation {
+    private String[] ht;
+    private int ptr;
+
+    HashImplementation(int size) {
+        ht = new String[size];
+        ptr = 0;
+    }
+
+    public void put(String key, String data) {
+        ht[Integer.parseInt(key)] = data;
+    }
+}
+
+
 public class HashEx {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println(">>> ");
 
         String keyStr = sc.nextLine();
-        
+
         System.out.println();
     }
 }
