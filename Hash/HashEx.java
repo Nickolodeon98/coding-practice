cd27591b6c2d0ea10eb142717dbe622df3112d16
Nickolodeon98 <sjeon0730@gmail.com>
2022-10-01 23:24:31 +0900

    Main class added

--- Hash/HashEx.java
+++ Hash/HashEx.java
@@ -1,2 +1,15 @@
-package Hash;public class HashEx {
+package Hash;
+
+import java.util.Hashtable;
+import java.util.Scanner;
+
+public class HashEx {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(">>> ");
+
+        String keyStr = sc.nextLine();
+        
+        System.out.println();
+    }
 }
