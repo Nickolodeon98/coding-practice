a2c12505199e19fd70f5fcaab268903a895ba7dd
Nickolodeon98 <sjeon0730@gmail.com>
2022-10-03 23:57:06 +0900

    RollingHash class added to implement rolling_hash

---
+++ Hash/HashEX2.java
@@ -0,0 +1,23 @@
+package Hash;
+
+/*Quoted from Hash_Table_Interviews_In_Java.pdf*/
+class RollingHash {
+    public static int stringHash(String str, int modulus) {
+        int kMult = 997;
+
+        int val = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            val = (val * kMult + c) % modulus;
+        }
+        return val;
+    }
+}
+
+public class HashEX2 {
+    public static void main(String[] args) {
+        int ret = RollingHash.stringHash("ABC", 3);
+
+        System.out.println(ret);
+    }
+}
