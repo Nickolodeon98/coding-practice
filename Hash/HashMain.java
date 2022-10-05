d16b89b5df31fb5b572fb6af4a2ddd66499a2cc6
Nickolodeon98 <sjeon0730@gmail.com>
2022-10-06 00:03:58 +0900

    AdvancedHashTable class tested

--- Hash/HashMain.java
+++ Hash/HashMain.java
@@ -7,5 +7,13 @@ public class HashMain {
         test.put(3, 10);
 
         System.out.println(test.get(3));
+
+        AdvancedHashTable test2 = new AdvancedHashTable();
+
+        test2.put(20, 100);
+        test2.put(30, 120);
+        test2.remove(30);
+
+        System.out.println(test2.get(30));
     }
 }
